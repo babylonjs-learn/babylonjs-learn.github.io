e37044d90582c53bc7f98ef6d483f301a65d5236
fish-ball <57082212@qq.com>
2018-12-22 23:50:09 +0800

    翻译了目录和简介

---
+++ __project__/static/js/script.js
@@ -0,0 +1,7 @@
+jQuery(function ($) {
+    console.log('============================================================');
+    console.log('<< Translate by babylonjs-learn. Contribution is welcome! >>');
+    console.log('============================================================');
+    // 注入样式
+    $('head').append('<link href="/__project__/static/css/style.css" rel="stylesheet" type="text/css" />');
+});
