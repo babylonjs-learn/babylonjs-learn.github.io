b0b13f5317623dd8d686c4d98b8567dd5e48c477
fish-ball <57082212@qq.com>
2018-12-31 14:30:43 +0800

    提交翻译章节

--- __project__/static/js/script.js
+++ __project__/static/js/script.js
@@ -1,7 +1,10 @@
-jQuery(function ($) {
-    console.log('============================================================');
-    console.log('<< Translate by babylonjs-learn. Contribution is welcome! >>');
-    console.log('============================================================');
-    // 注入样式
-    $('head').append('<link href="/__project__/static/css/style.css" rel="stylesheet" type="text/css" />');
-});
+console.log('============================================================');
+console.log('<< Translate by babylonjs-learn. Contribution is welcome! >>');
+console.log('============================================================');
+
+
+// 注入样式
+document.head.innerHTML += '<link href="/__project__/static/css/style.css" rel="stylesheet" type="text/css" />';
+
+// jQuery(function ($) {
+// });
